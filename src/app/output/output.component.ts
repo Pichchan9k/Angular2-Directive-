07378f64b019774888ae71abee7cbc8a17a7e456
Pichchan9k <pichchanok.n@hotmail.com>
2016-09-28 16:44:05 +0800

    combine all directive

---
+++ src/app/output/output.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-output',
+  templateUrl: './output.component.html',
+  styleUrls: ['./output.component.css']
+})
+export class OutputComponent implements OnInit {
+
+  @Input()
+  input
+
+  @Output()
+  outputBack
+
+  counter = 0;
+  constructor() {
+    this.outputBack = new EventEmitter<String>();
+   }
+
+  ngOnInit() {
+  }
+
+  sendBack(){
+    this.outputBack.emit(2)
+  }
+
+
+}
